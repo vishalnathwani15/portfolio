d4559d9b2dce5c41cec7a53cc182d69157589e8c
Vishal <vishal@gmail.com>
2024-12-15 20:03:54 +0530

    form creation

--- pages/Contact_Us.py
+++ pages/Contact_Us.py
@@ -1,3 +1,10 @@
 import streamlit as st
 
-st.title("Contact me for the same")
+st.title("Contact me")
+
+with st.form(key="my_forms"):
+    user_email =st.text_input(" your email address:")
+    message = st.text_area("Your message:")
+    button = st.form_submit_button("submit")
+    if button:
+        print("submited")
