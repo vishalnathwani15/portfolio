81ad3c2a5356c02d1d6c589537df2b6e66eadf58
Vishal <vishal@gmail.com>
2024-12-15 12:43:48 +0530

    add description image and link

--- main.py
+++ main.py
@@ -19,14 +19,20 @@ with col2:
 
 st.write("Below you can find some of apps I have built in python. Feel free to contact me!")
 
-col3,col4 = st.columns(2)
+col3,empty_col,col4 = st.columns([2,0.5,2])
 
 df = pandas.read_csv("data.csv",sep=";")
 
 with col3:
     for index,row in df[:10].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source Code]({row['url']})")
 
 with col4:
     for index,row in df[10:].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source Code]({row['url']})")
